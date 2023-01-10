d69c7aae87a23b38b7f43437dc59bbfb07521000
Dmitriy Afanasiev <afanasiev.dimon@outlook.com>
2023-01-10 23:10:03 +0100

    added cents

--- src/components/PriceInput/PriceInput.jsx
+++ src/components/PriceInput/PriceInput.jsx
@@ -1,17 +1,24 @@
 import css from './PriceInput.module.css';
 import sprite from '../../images/svg/sprite.svg';
 
-function PriceInput(){
-  return(
+function PriceInput() {
+  return (
     <div className={css.container}>
-      <input id='amount' className={css.input} type='number' min='0' step='1'  placeholder='00' required/>
+      <input
+        id="amount"
+        className={css.input}
+        type="number"
+        step="0.01"
+        placeholder="00.00"
+        required
+      />
       <div className={css.calcContainer}>
-        <svg width='20' height='20'>
-          <use href={`${sprite}#icon-calculator`}></use>  
+        <svg width="20" height="20">
+          <use href={`${sprite}#icon-calculator`}></use>
         </svg>
       </div>
     </div>
-  )
+  );
 }
 
 export default PriceInput;
